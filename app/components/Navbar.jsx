bf7c9d8c943f4b41ce921348af7e04b77f184a23
gdvl777 <gdvl777@gmail.com>
2026-02-04 23:19:47 -0500

    css

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -37,7 +37,7 @@ export default function Navbar() {
             src="/logo_DataConSentido.png"
             alt="DataConSentido"
           />
-          <span className="brandText">DataConSentido</span>
+          <span className="brandText"></span>
         </a>
 
         {/* Desktop */}
